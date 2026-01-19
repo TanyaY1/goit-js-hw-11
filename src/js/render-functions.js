89d7b7f48f06b44a2d8b2165c39467c2043012b9
Tanya <yakovetsta1@gmail.com>
2026-01-18 21:41:32 -0400

    updates

---
+++ src/js/render-functions.js
@@ -0,0 +1,36 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryContainer = document.querySelector(".gallery");
+
+const lightbox = new SimpleLightbox(".gallery a");
+
+export function createGallery(images) {
+  const markup = images
+    .map(
+      (img) => `
+    <li>
+      <a href="${img.largeImageURL}">
+        <img src="${img.webformatURL}" alt="${img.tags}" loading="lazy"/>
+      </a>
+      <p>Likes: ${img.likes} | Views: ${img.views} | Comments: ${img.comments} | Downloads: ${img.downloads}</p>
+    </li>
+  `
+    )
+    .join("");
+
+  galleryContainer.insertAdjacentHTML("beforeend", markup);
+  lightbox.refresh(); 
+}
+
+export function clearGallery() {
+  galleryContainer.innerHTML = "";
+}
+
+export function showLoader() {
+  galleryContainer.classList.add("loading");
+}
+
+export function hideLoader() {
+  galleryContainer.classList.remove("loading");
+}
--- src/main.js
+++ src/main.js
@@ -1,11 +1,47 @@
-import { getImagesByQuery } from "./js/pixabay-api";
+import { getImagesByQuery } from "./js/pixabay-api.js";
+import { createGallery, clearGallery, showLoader, hideLoader } from "./js/render-functions.js";
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
 
-getImagesByQuery("cats")
-  .then(response => {
-    console.log(response.data.hits);
-  })
-  .catch(error => {
-    console.log(error);
-  });
 
-//
+const form = document.querySelector(".form");
+const input = form.querySelector("input[name='search-text']");
+
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  const query = input.value.trim();
+
+  if (!query) {
+    iziToast.warning({
+      title: "Warning",
+      message: "Please enter a search term!",
+    });
+    return;
+  }
+
+  clearGallery();
+  showLoader();
+
+  try {
+    const response = await getImagesByQuery(query);
+    const images = response.data.hits;
+
+    if (images.length === 0) {
+      iziToast.info({
+        title: "No results",
+        message: "Sorry, there are no images matching your search query. Please try again!",
+      });
+    } else {
+      createGallery(images);
+    }
+  } catch (error) {
+    console.error(error);
+    iziToast.error({
+      title: "Error",
+      message: "Something went wrong. Please try again later!",
+    });
+  } finally {
+    hideLoader();
+  }
+});
